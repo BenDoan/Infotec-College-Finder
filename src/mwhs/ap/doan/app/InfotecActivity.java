cc1cc887cc21d30866b16df4a3eff5200d047fe5
Ben Doan <bendoan5@gmail.com>
2012-03-05 15:18:14 -0600

    initial commit

---
+++ src/mwhs/ap/doan/app/InfotecActivity.java
@@ -0,0 +1,13 @@
+package mwhs.ap.doan.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfotecActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
