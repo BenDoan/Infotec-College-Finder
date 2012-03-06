dda80ba2598c1fdd049e5ced8edc26b6889a24fb
Ben Doan <bendoan5@gmail.com>
2012-03-06 14:01:59 -0600

    added eclipse files

---
+++ src/mwhs/ap/bkat/app/InfotecActivity.java
@@ -0,0 +1,14 @@
+package mwhs.ap.bkat.app;
+
+import mwhs.ap.doan.app.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfotecActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
