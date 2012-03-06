5b2ddc3649da31039b19ff23ba8735b5e64ba978
unknown <khotovy935@WHS-CSI-204-14.MILLARDPS.NET>
2012-03-06 14:53:40 -0600

    tested spinner things

--- gen/mwhs/ap/doan/app/R.java
+++ gen/mwhs/ap/doan/app/R.java
@@ -8,16 +8,49 @@
 package mwhs.ap.doan.app;
 
 public final class R {
+    public static final class array {
+        public static final int sports_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int housing1=0x7f06000e;
+        public static final int housing2=0x7f06000f;
+        public static final int linearLayout2=0x7f060000;
+        public static final int llHousing=0x7f06000d;
+        public static final int llMajor=0x7f060001;
+        public static final int llPopulation=0x7f060007;
+        public static final int llRegion=0x7f060010;
+        public static final int llSports=0x7f060004;
+        public static final int lltuition=0x7f06000a;
+        public static final int major1=0x7f060002;
+        public static final int major2=0x7f060003;
+        public static final int population1=0x7f060008;
+        public static final int population2=0x7f060009;
+        public static final int region1=0x7f060011;
+        public static final int region2=0x7f060012;
+        public static final int spinner=0x7f060013;
+        public static final int sports1=0x7f060005;
+        public static final int sports2=0x7f060006;
+        public static final int tuition1=0x7f06000b;
+        public static final int tuition2=0x7f06000c;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int results_list=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int housing=0x7f040006;
+        public static final int major=0x7f040002;
+        public static final int population=0x7f040004;
+        public static final int region=0x7f040007;
+        public static final int selectionSports=0x7f040008;
+        public static final int sports=0x7f040003;
+        public static final int tuition=0x7f040005;
     }
 }
