9d6a19042b753759e219230933c0e4f42e2d1ee7
unknown <khotovy935@WHS-CSI-204-14.MILLARDPS.NET>
2012-03-07 14:14:26 -0600

    the search button opens a results page (atleast it should)

--- src/mwhs/ap/bkat/app/InfotecActivity.java
+++ src/mwhs/ap/bkat/app/InfotecActivity.java
@@ -2,15 +2,20 @@ package mwhs.ap.bkat.app;
 
 import mwhs.ap.doan.app.R;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class InfotecActivity extends Activity {
+public class InfotecActivity extends Activity implements android.view.View.OnClickListener {
+	private Button search;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,11 @@ public class InfotecActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
+        search = (Button) findViewById(R.id.search);
+        search.setOnClickListener(this);
+        
+        
     }
     
     public class MyOnItemSelectedListener implements OnItemSelectedListener {
@@ -40,4 +50,16 @@ public class InfotecActivity extends Activity {
           // Do nothing.
         }
     }
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.search:{
+			Intent i = new Intent().setClass(this, SearchResults.class);
+			startActivity(i);
+		}
+		}
+		
+	}
+
 }
