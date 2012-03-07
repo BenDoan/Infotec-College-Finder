070c91d46f1571aa7feaf55d68243f0c0bbb77a3
unknown <aallen700@WHS-MEDIA2-19.MILLARDPS.NET>
2012-03-07 10:18:38 -0600

    update andrew

--- src/mwhs/ap/bkat/app/InfotecActivity.java
+++ src/mwhs/ap/bkat/app/InfotecActivity.java
@@ -17,12 +17,27 @@ public class InfotecActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerSports);
+        Spinner spinnersport = (Spinner) findViewById(R.id.spinnerSports);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.sports_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        spinnersport.setAdapter(adapter);
+        spinnersport.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
+        Spinner spinnertuition = (Spinner) findViewById(R.id.spinnerTuition);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
+                this, R.array.tuition_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnertuition.setAdapter(adapter1);
+        spinnertuition.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
+        Spinner spinnerregion = (Spinner) findViewById(R.id.spinnerRegion);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+                this, R.array.region_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerregion.setAdapter(adapter2);
+        spinnerregion.setOnItemSelectedListener(new MyOnItemSelectedListener());
+        
     }
     
     public class MyOnItemSelectedListener implements OnItemSelectedListener {
