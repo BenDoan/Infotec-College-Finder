36902dac6e52801606b1aa5533b1e79367a12f4e
Andrew <bendoan5@gmail.com>
2012-03-07 14:56:49 -0600

    up

--- gen/mwhs/ap/doan/app/R.java
+++ gen/mwhs/ap/doan/app/R.java
@@ -42,6 +42,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int results_list=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
